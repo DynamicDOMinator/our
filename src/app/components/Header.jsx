fdc82a6be7c2cfce4b04357787e81c2b4208fb01
Ahmed <ahmedsayedhamed38@gmail.com>
2025-07-11 02:03:13 +0300

    ASfd

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -92,7 +92,7 @@ export default function Header() {
       )}
 
       <div className="flex fixed w-full items-center justify-between pt-10 md:px-16 px-8 font-medium text-lg z-50">
-        <div className="">
+        <div className={`transition-all duration-500 ${isScrolled ? 'md:opacity-100 opacity-0 md:transform-none transform -translate-y-10' : 'opacity-100 transform-none'}`}>
           <Image src={"/Techshun.png"} height={40} width={140} alt="Techshun" />
         </div>
 
