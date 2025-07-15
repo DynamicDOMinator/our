12d87bd3e3d0f781c3b4b5d626aebeb66881d3be
Ahmed <ahmedsayedhamed38@gmail.com>
2025-07-15 03:44:00 +0300

    SADf

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,11 @@ export const metadata = {
   title: "PROSENTAL",
   description:
     "Techshun is a full-service digital agency specializing in web and mobile app development, UI/UX design, SaaS platforms, cloud integration, and custom software. We transform innovative ideas into scalable digital experiences using cutting-edge technology and agile practices.",
+  icons: {
+    icon: "/fav.png",
+    apple: "/fav.png",
+    shortcut: "/fav.png",
+  },
 };
 
 export default function RootLayout({ children }) {
